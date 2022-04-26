036747078c482e885225e7fe298551d8effc7452
Lucas Machioni Jorge <101301679+LucasMachioni@users.noreply.github.com>
2022-04-25 22:32:34 -0300

    Adicionando botão de remover tarefa

--- todo-list.js
+++ todo-list.js
@@ -21,9 +21,26 @@ const minhaTagUl = document.getElementById("lista-de-tarefas")
 //Criando a tag LI com JavaScript
 const criarTagLI = document.createElement("li")
 
+//Criando uma tag em negrito
+const tagRemover = "<b onclick='removerItem(event)' > Remover </b>"
+
 //Adicionando um texto para nossa tag li criada
-criarTagLI.innerText = valorInput
+criarTagLI.innerHTML = valorInput + tagRemover;
+
+console.log(criarTagLI);
 
 minhaTagUl.appendChild(criarTagLI)
 
+console.log('é')
+
+}
+
+const removerItem = (event)=> {
+
+  //Pegando a tag <b> dentro do nosso evento
+  const tagRemover = event.target;
+
+  console.log("Cliquei na função remover")
+
+  console.log(tagRemover)
 }
